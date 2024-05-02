9d0c8fb4340fc3cdb6b47d2a89261935fd8a8704
Yuri Gerasimchuk <y.gerasimchuk@gmail.com>
2024-05-02 10:37:46 +0300

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Yuri Gerasimchuk on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
