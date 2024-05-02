5acfb9e62dfea19e38d8a11f2fe8abda987c9469
ygeras <y.gerasimchuk@gmail.com>
2024-05-02 16:43:39 +0300

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Yuri Gerasimchuk on 02.05.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
