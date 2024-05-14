d6e8361d1d1152e3105c796443e01acc04849ea1
ygeras <y.gerasimchuk@gmail.com>
2024-05-14 09:55:17 +0300

    [Feature] Add Settings View

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Yuri Gerasimchuk on 14.05.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
