747410246c4f9b5ee11221ef70c828cbe2b6db4f
ygeras <y.gerasimchuk@gmail.com>
2024-05-15 11:09:07 +0300

    [Feature] Update Settings View

--- SwiftfulSourceControl/SettingsView.swift
+++ SwiftfulSourceControl/SettingsView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State private var doSomething: Bool = false
+    @State private var doSomething: Bool = true
     
     var body: some View {
-        Text("Settings View")
+        Text("Settings")
     }
 }
 
